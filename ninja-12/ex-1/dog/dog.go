590a5da2d0ba1364f34ad0f373c99c0612119a43
Eddie Sheffield <eddie.sheffield@block.one>
2019-02-13 16:16:53 -0500

    Tweaked docs

--- ninja-12/ex-1/dog/dog.go
+++ ninja-12/ex-1/dog/dog.go
@@ -1,7 +1,7 @@
 // Package dog contains utilities related to dogs
 package dog
 
-// Function Years converts human years to dog years
+// Years converts human years to dog years
 func Years(human int) int {
 	return 7 * human
 }
